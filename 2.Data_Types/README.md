3a2a94d90a2364ea77978bb10836d2dcc97a25ff
ds-joy <debasishsarker36@gmail.com>
2019-07-14 19:56:15 +0600

    commiting 2.Data_Types folder

---
+++ 2.Data_Types/README.md
@@ -0,0 +1,8 @@
+There are six primitive data types in javascript
+1.Number  (Floting point numbers for decimals  and integers)
+2.Strings (Sequence of characters)
+3.Boolean (Logical data type. true/false)
+4.Undefined (Data type of a veriable that does not have a value yet)
+5.Null (Means non-existing)
+
+here we would like to assign these five types of values to veriables and see the output in the console
---
+++ 2.Data_Types/dataTypes.js
@@ -0,0 +1,21 @@
+var firstName = 'John'; // String type
+var lastName = 'Smith'; // String type
+
+var age = 26; // Number type
+var adult = true; // Boolean type
+
+var hasJob; // Undefined
+
+console.log("What is your name?");
+console.log(firstName + " " + lastName);
+
+console.log("How old are you?");
+console.log(age);
+
+console.log("Are you an adult person?");
+console.log(adult);
+
+console.log("Do you have a job?");
+console.log(hasJob);
+
+
